454af509c485af21050a35a2c6d33d791096ec24
Laz <nyys811@aol.com>
2023-07-29 01:42:40 -0400

    started on thought controller, update a thought, delete a thought

---
+++ controllers/thoughtController.js
@@ -0,0 +1,47 @@
+const Thought = require('../models/Thought');
+const User = require('../models/User');
+
+
+exports.createThought = async (req, res) => {
+  try {
+    const { thoughtText, username } = req.body;
+    const newThought = await Thought.create({ thoughtText, username });
+    const user = await User.findOneAndUpdate(
+      { username },
+      { $push: { thoughts: newThought._id } },
+      { new: true }
+    );
+    res.json(newThought);
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+};
+
+exports.updateThought = async (req, res) => {
+  try {
+    const updatedThought = await Thought.findByIdAndUpdate(
+      req.params.thoughtId,
+      req.body,
+      { new: true }
+    );
+    if (!updatedThought) {
+      return res.status(404).json({ message: 'Thought not found' });
+    }
+    res.json(updatedThought);
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+};
+
+exports.deleteThought = async (req, res) => {
+  try {
+    const deletedThought = await Thought.findByIdAndDelete(req.params.thoughtId);
+    if (!deletedThought) {
+      return res.status(404).json({ message: 'Thought not found' });
+    }
+    await User.updateMany({}, { $pull: { thoughts: req.params.thoughtId } });
+    res.json(deletedThought);
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+};
