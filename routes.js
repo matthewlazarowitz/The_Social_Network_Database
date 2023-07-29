1ce6529b6c793db79ed22d788c3c6fa220811936
Laz <nyys811@aol.com>
2023-07-29 01:45:21 -0400

    added all the user routes

--- routes.js
+++ routes.js
@@ -2,3 +2,11 @@ const express = require('express');
 const router = express.Router();
 const userController = require('./controllers/userController'); 
 const thoughtController = require('./controllers/thoughtController'); 
+
+router.get('/users', userController.getAllUsers);
+router.get('/users/:userId', userController.getUserById);
+router.post('/users', userController.createUser);
+router.put('/users/:userId', userController.updateUser);
+router.delete('/users/:userId', userController.deleteUser);
+router.post('/users/:userId/friends/:friendId', userController.addFriend);
+router.delete('/users/:userId/friends/:friendId', userController.removeFriend);
