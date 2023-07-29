169638b46f3f523c21c07113b406e2424fe2ae23
Laz <nyys811@aol.com>
2023-07-29 01:46:05 -0400

    finished routes with thought routes

--- routes.js
+++ routes.js
@@ -10,3 +10,11 @@ router.put('/users/:userId', userController.updateUser);
 router.delete('/users/:userId', userController.deleteUser);
 router.post('/users/:userId/friends/:friendId', userController.addFriend);
 router.delete('/users/:userId/friends/:friendId', userController.removeFriend);
+
+router.post('/thoughts', thoughtController.createThought);
+router.put('/thoughts/:thoughtId', thoughtController.updateThought);
+router.delete('/thoughts/:thoughtId', thoughtController.deleteThought);
+router.post('/thoughts/:thoughtId/reactions', thoughtController.createReaction);
+router.delete('/thoughts/:thoughtId/reactions/:reactionId', thoughtController.removeReaction);
+
+module.exports = router;
