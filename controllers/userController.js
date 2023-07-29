879bdba6917e0bfa32b0db8c8d9a82d8b1392ac4
Laz <nyys811@aol.com>
2023-07-29 01:40:12 -0400

    added last two controllers, to add a freind, and remove a friend

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,63 +2,93 @@ const User = require('../models/User');
 
 
 exports.getAllUsers = async (req, res) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (error) {
-    res.status(500).json({ error: 'Server error' });
-  }
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ error: 'Server error' });
+    }
 };
 
 exports.getUserById = async (req, res) => {
     try {
-      const user = await User.findById(req.params.userId)
-        .populate('thoughts')
-        .populate('friends');
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-      res.json(user);
+        const user = await User.findById(req.params.userId)
+            .populate('thoughts')
+            .populate('friends');
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(user);
     } catch (error) {
-      res.status(500).json({ error: 'Server error' });
+        res.status(500).json({ error: 'Server error' });
     }
-  };
-  
+};
 
-  exports.createUser = async (req, res) => {
+
+exports.createUser = async (req, res) => {
     try {
-      const { username, email } = req.body;
-      const newUser = await User.create({ username, email });
-      res.json(newUser);
+        const { username, email } = req.body;
+        const newUser = await User.create({ username, email });
+        res.json(newUser);
     } catch (error) {
-      res.status(500).json({ error: 'Server error' });
+        res.status(500).json({ error: 'Server error' });
     }
-  };
-  
-  exports.updateUser = async (req, res) => {
+};
+
+exports.updateUser = async (req, res) => {
     try {
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.userId,
-        req.body,
-        { new: true }
-      );
-      if (!updatedUser) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-      res.json(updatedUser);
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.userId,
+            req.body,
+            { new: true }
+        );
+        if (!updatedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(updatedUser);
     } catch (error) {
-      res.status(500).json({ error: 'Server error' });
+        res.status(500).json({ error: 'Server error' });
     }
-  };
-  
-  exports.deleteUser = async (req, res) => {
+};
+
+exports.deleteUser = async (req, res) => {
     try {
-      const deletedUser = await User.findByIdAndDelete(req.params.userId);
-      if (!deletedUser) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-      res.json(deletedUser);
+        const deletedUser = await User.findByIdAndDelete(req.params.userId);
+        if (!deletedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(deletedUser);
     } catch (error) {
-      res.status(500).json({ error: 'Server error' });
+        res.status(500).json({ error: 'Server error' });
     }
-  };
+};
+
+exports.addFriend = async (req, res) => {
+    try {
+        const { userId, friendId } = req.params;
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        user.friends.push(friendId);
+        await user.save();
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ error: 'Server error' });
+    }
+};
+
+exports.removeFriend = async (req, res) => {
+    try {
+        const { userId, friendId } = req.params;
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        user.friends.pull(friendId);
+        await user.save();
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ error: 'Server error' });
+    }
+};
