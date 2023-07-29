8c1528833c46f7667f71b82a681aaffd5bea8886
Laz <nyys811@aol.com>
2023-07-29 01:56:46 -0400

    added last routes for, create reaction, delete reaction, add friend, remove friend. also lastly call to start server

--- app.js
+++ app.js
@@ -114,3 +114,72 @@ app.delete('/api/thoughts/:id', (req, res) => {
       res.status(404).json({ message: 'Thought not found' });
     }
   });
+  
+app.post('/api/thoughts/:thoughtId/reactions', (req, res) => {
+    const thoughtId = parseInt(req.params.thoughtId);
+    const thought = thoughts.find((thought) => thought.id === thoughtId);
+  
+    if (thought) {
+      const newReaction = { reactionId: new mongoose.Types.ObjectId(), ...req.body, createdAt: new Date() };
+      thought.reactions.push(newReaction);
+      res.status(201).json(newReaction);
+    } else {
+      res.status(404).json({ message: 'Thought not found' });
+    }
+  });
+  
+app.delete('/api/thoughts/:thoughtId/reactions/:reactionId', (req, res) => {
+    const thoughtId = parseInt(req.params.thoughtId);
+    const reactionId = req.params.reactionId.toString(); // Convert the reactionId to a string
+  
+    const thought = thoughts.find((thought) => thought.id === thoughtId);
+  
+    if (thought) {
+      const reactionIndex = thought.reactions.findIndex((reaction) => reaction.reactionId.toString() === reactionId); // Compare as strings
+      if (reactionIndex !== -1) {
+        thought.reactions.splice(reactionIndex, 1);
+        res.json({ message: 'Reaction deleted successfully' });
+      } else {
+        res.status(404).json({ message: 'Reaction not found' });
+      }
+    } else {
+      res.status(404).json({ message: 'Thought not found' });
+    }
+  });
+  
+app.post('/api/users/:userId/friends/:friendId', (req, res) => {
+    const userId = parseInt(req.params.userId);
+    const friendId = parseInt(req.params.friendId);
+    const user = users.find((user) => user.id === userId);
+    const friend = users.find((user) => user.id === friendId);
+  
+    if (user && friend) {
+      user.friends.push(friendId);
+      res.json(user);
+    } else {
+      res.status(404).json({ message: 'User or friend not found' });
+    }
+  });
+  
+app.delete('/api/users/:userId/friends/:friendId', (req, res) => {
+    const userId = parseInt(req.params.userId);
+    const friendId = parseInt(req.params.friendId);
+    const user = users.find((user) => user.id === userId);
+  
+    if (user) {
+      const friendIndex = user.friends.indexOf(friendId);
+      if (friendIndex !== -1) {
+        user.friends.splice(friendIndex, 1);
+        res.json(user);
+      } else {
+        res.status(404).json({ message: 'Friend not found in user\'s friend list' });
+      }
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+  
